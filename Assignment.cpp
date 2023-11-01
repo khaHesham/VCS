ad4143f00b88a2b9f82c5e519c271a281f44d143
khaled hesham <kha.hesham@gmail.com>
2023-11-01 22:10:36 +0200

    Adding getAverage function

--- Assignment.cpp
+++ Assignment.cpp
@@ -13,10 +13,17 @@ int getSum(vector<int> &A)
     return sum;
 }
 
+float getAverage(vector<int> &A)
+{
+    return (float)getSum(A) / A.size();
+}
+
 int main()
 {
     vector<int> A = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // 55
     cout << getSum(A) << endl;
+
+    cout << getAverage(A) << endl;
 
     return 0;
 }
